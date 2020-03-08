5c39f1189f0e4e0434124d3acd77fd3b0c2924c6
Khirodkar, Puja | Puja | ESD <puja.khirodkar@rakuten.com>
2020-03-08 18:50:45 +0900

    City Name accessible with change in API

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ type Props = {}
 type State = {
     loading: boolean,
     data?: {
+        city: string,
         iconName: string,
         title: string,
         description: string,
@@ -21,26 +22,38 @@ type State = {
         humidity: number,
         minTemp: number,
         maxTemp: number
-
     }
 
+}
+type Position = {
+    lat: number
+    lon: number
 }
 
 class App extends React.Component<Props, State> {
 
-    constructor(props: Readonly<Props>) {
+    constructor(props: Props) {
         super(props);
         this.state = {
             loading: true
         }
     }
 
-    componentDidMount(): void {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&appid=7ea90f3cafa8cefea2d866032a9d944b&units=metric")
-            .then(response => response.json())
-            .then(
-                data => {
+    loadWeatherData(position: Position) {
+        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + position.lat
+            + "&lon=" + position.lon
+            + "&appid=7ea90f3cafa8cefea2d866032a9d944b&units=metric";
+        console.log("URL=>" + apiUrl);
+
+        fetch(apiUrl)
+            .then(response => {
+                console.log(response);
+                return response.json()
+            })
+            .then(data => {
+                    console.log(data);
                     const resultObj = {
+                        city: data['name'],
                         iconName: IconUtils.getIconNameById(data['weather'][0]['id']),
                         title: data['weather'][0]['main'],
                         description: data['weather'][0]['description'],
@@ -48,23 +61,32 @@ class App extends React.Component<Props, State> {
                         pressure: data['main']['pressure'],
                         humidity: data['main']['humidity'],
                         minTemp: data['main']['temp_min'],
-                        maxTemp: data['main']['temp_max']
+                        maxTemp: data['main']['temp_max'],
                     };
-
+                    console.log("City=>" + data['name']);
                     this.setState({
                         loading: false,
                         data: resultObj
                     });
-                    console.log(data['weather']);
-                    console.log(data.weather);
-                    console.log(JSON.stringify(data, null, 3))
                 }
             ).catch(error => {
                 console.error(error);
             }
-        ).finally(() => {
+        )
+    }
 
-        })
+    getPosition = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let coords = {
+                lat: position.coords.latitude,
+                lon: position.coords.longitude
+            };
+            this.loadWeatherData(coords);
+        });
+    };
+
+    componentDidMount(): void {
+        this.getPosition();
     }
 
     render() {
@@ -74,6 +96,7 @@ class App extends React.Component<Props, State> {
             const data = this.state.data!!;
             return (
                 <div className="App">
+                    <span className="city">{data.city}</span>
                     <Row className="main-component">
                         <MainComponent id={1} title={data.title} iconName={data.iconName}
                                        temperature={data.temperature}
@@ -83,7 +106,7 @@ class App extends React.Component<Props, State> {
                         <Row noGutters={true}>
                             <Col xs={12} md={6} className="sub-component">
                                 <TemperatureComponent minTemp={data.minTemp}
-                                                                                                maxTemp={data.maxTemp}/>
+                                                      maxTemp={data.maxTemp}/>
                             </Col>
                             <Col xs={6} md={3} className="sub-component">
                                 <PressureComponent pressure={data.pressure}/>
